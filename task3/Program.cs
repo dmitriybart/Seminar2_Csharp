86d46b09309c9d55b048609a56ec61a24d9a9bf0
Dmitriy B <kupperfeet@gmail.com>
2022-08-16 21:12:43 +0300

    task 3 done

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,1 +1,13 @@
 ﻿// Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Введите пятизначное число: ");
+int UserNumber = int.Parse(Console.ReadLine());
+int divUserNumber = UserNumber/1000;
+int Recursiv = (UserNumber%10*10)+(UserNumber%100/10);
+if (divUserNumber==Recursiv)
+{
+    Console.WriteLine("Число " +UserNumber+ " является полиндромом !");
+}
+else 
+{
+    Console.WriteLine("Число " +UserNumber+ " НЕ является полиндромом !");
+}
