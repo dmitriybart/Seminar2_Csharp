c6defe57c1844c30975fc07a6b507183cf30c9f7
Dmitriy B <kupperfeet@gmail.com>
2022-08-16 21:42:26 +0300

    task4 done

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// показать таблицу квадратов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int count = 0;
+int i = 1;
+while (count < n);
+{
+    int result = i*i;
+    Console.Write(result + " ");
+    count++;
+    i++;
+}
