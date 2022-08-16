90d909b8e1db0173db3a2d6e1416efbce315afdc
Dmitriy B <kupperfeet@gmail.com>
2022-08-16 22:48:17 +0300

    task5 done

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿//возведите число А в степень В, все числа натуральные
+Console.Write("Введите основание: ");
+int a = int.Parse (Console.ReadLine());
+Console.Write("Введите степень: ");
+int b = int.Parse (Console.ReadLine());
+int count = 1;
+int sum = a;
+if (count<=b)
+{while (count < b)
+{
+    sum = sum*a;
+    count++;
+}
+Console.WriteLine("Ответ: "+sum);
+}
+else 
+Console.WriteLine("Ответ: 1");
