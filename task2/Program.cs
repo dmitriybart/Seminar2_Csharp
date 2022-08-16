64c7ab93714cd875047b0adabf05459ef2710862
Dmitriy B <kupperfeet@gmail.com>
2022-08-16 14:13:23 +0300

    task 2 done

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите координату Х: ");
+int x = int.Parse (Console.ReadLine());
+
+Console.Write("Введите координату Y: ");
+int y = int.Parse (Console.ReadLine());
+
+if (x!=0 && y!=0)
+{
+    if (x > 0 && y > 0)
+    {
+    Console.WriteLine("Первая четверть.");
+    }
+    if (x < 0 && y > 0)
+    {
+    Console.WriteLine("Вторая четверть.");
+    }
+    if (x < 0 && y < 0)
+    {
+    Console.WriteLine("Третья четверть");
+    }
+    if (x > 0 && y < 0)
+    {
+    Console.WriteLine("Четвёртая четверть");
+    }
+}
+else
+{
+    Console.WriteLine("Точка не принадлежит ни одной из четвертей плоской системы координат !");
+}
+
